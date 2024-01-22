255664da960f335fe4348d4d2899ed547aba2dd4
Tim <tim.smith2814@gmail.com>
2024-01-22 15:41:11 +0000

    Wink wink

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,7 @@ export default function Home() {
         <Link href="/dance">Home</Link>
       </h2>
       <p>This is my amazing skeleton. Look at it go!</p>
+      <p>But where is the skeleton ;)</p>
     </div>
   );
 }
